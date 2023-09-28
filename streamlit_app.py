4a15afd5d0368f7647e0c76cec1cf5777a2998c6
akshay-299 <146323689+akshay-299@users.noreply.github.com>
2023-09-28 15:38:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,4 +16,4 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 
 streamlit.multiselect("Pick Some Fruits:", list(my_fruit_list.index))
 
-#streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(my_fruit_list)
