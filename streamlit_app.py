0f8725a9f1f25d50fba0fbef4e46b20eb86156ad
akshay-299 <146323689+akshay-299@users.noreply.github.com>
2023-10-04 10:58:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,3 +30,10 @@ normalize = pandas.json_normalize(responce.json())
 streamlit.dataframe(normalize)
 
 import snowflake.connector
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
